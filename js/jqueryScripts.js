f3694025e6c16616aaee9ce670e1286553d2d4bd
AlexSh20 <Alex.OnChoice@yandex.ru>
2020-11-15 18:27:39 +0300

    Добавил комментарии к функциям

--- js/jqueryScripts.js
+++ js/jqueryScripts.js
@@ -10,13 +10,13 @@ function addShopsList() {
     $('#addShopHere').load('ajax/shops.html');
     $(".section-shops-button").hide();
 }
-
+//функция, которая открывает сворачивает магазинов
 function hideShopsList() {
     $(".shop-List").slideUp();
     $(".section-shops-button-hide").slideUp();
     $(".section-shops-button-show").show();
 }
-
+//функция, которая открывает список магазинов
 function showShopsList() {
     $(".section-shops-button-show").hide();
     $(".shop-List").slideDown();
